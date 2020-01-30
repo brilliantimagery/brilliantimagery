6bd56cba2ef91f312886ce58e931b284d6d7e9bd
Chad DeRosier <chad.derosier@gmail.com>
2020-01-30 00:15:35 -0800

    ppm passing tests

--- tests/unit/ppm/conftest.py
+++ tests/unit/ppm/conftest.py
@@ -6,8 +6,8 @@ import numpy as np
 
 @pytest.fixture()
 def setup_ppm_saving(data_folder_path):
-    test_data = np.load(str(data_folder_path / 'F-18.ppm.pickle.np'))
+    test_data = np.load(str(data_folder_path / 'ljpeg' / 'F-18.ppm.pickle.np'))
 
-    expected = np.fromfile(str(data_folder_path / 'ppm_F-18.ppm'), np.uint8).astype(np.intc)
+    expected = np.fromfile(str(data_folder_path / 'ppm' / 'ppm_F-18.ppm'), np.uint8).astype(np.intc)
 
     return test_data, expected
